5d4ef09467e30d559ff11fbd8bd4623f7565b148
Robert-Card <robertleandroc@gmail.com>
2023-09-21 02:04:28 -0300

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
   const [topFiveSongs, setTopFiveSongs]: TopSongs = useState({});
   const [artists, setArtists] = useState(null);
-  const token = 'BQAgqLY783mUU6DCOIY2O_FyCGyEU5XJiiQHaqJGcoO_MAl0kWwoi-UBL8ASzIsHIsJd1WqIe5JUkE9TBp0RqAjlL4gHFmTPAc1u3_1eeIT7rEYQ6AGZLo9NyIBkZe6jzpGTQBSApMQ_cQIIB11vqrqfjuYXvzEcEA1CAXBzps5P7nQUeWBse9UUqx6b__TYxLiRDNarWjnqvF5qIlBDh1zQPKBNeAHhCJNpmzipeecmQTEEXAj4vnQWHXXiAiaCgFoQ'
+  const token = 'BQDQ9oToEIumv5YoVYHj_hBEDISC8U57MMN1WI99N1-po8gat6xFdkLoYyEQoFZLFkS3BDYFkhwGiiGh5MpwqJ3aqjzGdVthqLfPc9XeWusyORcvkmmqUNlXfCiOAWvbwhD6ykHlOOSIW4LomUHbqpabRMnz-uGgRi_qrr749OnBJdEayxGmTZvnH0yt5K-4ToQP_kaxSHprztzxSE_J0W1dNjC1PFF5A0k6GaxovwGnLHdjBPUOnF3pi9iReVu81o2T';
   async function fetchWebApi(endpoint, method) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
@@ -32,19 +32,28 @@ function App() {
       type: 'artist', 
       q: "Madonna",
     })
-    const artists = await fetchWebApi('v1/search?${params}', 'GET');
+    const artists = await fetchWebApi(`v1/search?${params}`, 'GET');
     console.log(artists); 
   }
 
   return (
-    <div>
-      <button class="search" onClick={getTopTracks}>Pesquisar Top 3</button>
-        <div class="list">
-          {topFiveSongs.items?.map((item) => (
-            <p>{item.album.name}</p>
-          ))}
-        </div>
+    <div class='pageStyle'>
+      <div>
+        <button onClick={getTopTracks}>Pesquisar Top 3</button>
+            {topFiveSongs.items?.map((item) => (
+              <p>{item.album.name}</p>
+            ))}     
+      </div>
+
+              
+      <form action=''>
+              <label htmlFor='artist'>Nome do Artista</label>
+              <input type='text' name='' id='artist' />
+        <button onClick={searchForArtist}>Pesquisar Artistas</button>   
+      </form>
+      
     </div>
+    
   );
 }
 
