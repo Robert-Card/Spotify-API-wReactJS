510ffba9df9bbe1cfebc27cda790ed499500cb6f
Robert-Card <robertleandroc@gmail.com>
2023-09-21 21:03:28 -0300

    Update API Token

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
   const [topFiveSongs, setTopFiveSongs]: TopSongs = useState({});
   const [artist, setArtist] = useState({});
-  const token = 'BQC_MKrOED8iKLvY9hKkFKd2NSPcR6ZqE8oZK045Gse8uD3SK3pdniEVPzb3OwtGIMtOsh-i23sl-TnSaNCSYAbd1oKZNdkVTtISDiVTewDOzdikfqtl8Xd3k2z-GuCahqD2lIL4m0YSwDi3wV5mCM27hNsU-m09WUuPBD818Im-iVEBuuVUICxYLxUjnlvOXan1GFpxiZ-e_60nHXL4Ks1NF9ar7hEsoFfYJy-uCYPRhs_CuwHsWsopfNrwyWW7j0pH';
+  const token = 'BQDXfmrwnSsl7vXDq1klL4ga9EWLeudYwp6hEj1NWbhlu0gIih0crXJCGzRZiCPVhsnhG28YzEAP35yI7QjPY9XDJtFUaAxsvX-9Qva16ulk5xzWJsP7zCVqv_KNgaHaVtsWPp37PUJENJ54nzoyHpwtNmwL1o8oJdACG7ayMwnW3Akt2rR3eGZ-3oNfWf7el8C3TM0JT-yxwXRcMZ4bZFClGbnJdqCz0xrouP1eIhbJWFlD4_CjmwWU70sx1wfulGZo';
   async function fetchWebApi(endpoint, method) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
